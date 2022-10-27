0ce38cbfc6d251a1d621b9c5f825a947af9dec59
Cristian Gofiar <cristiangofiar@gmail.com>
2022-10-26 23:10:45 -0300

    Toqué tantas cosas que ni me acuerdo que hice

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -4,7 +4,7 @@ namespace PoC.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public bool Seen { get; set; }
+        public bool Seen { get; set; } = false;
         public string Image { get { return $"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{ID}.png"; } }
     }
 }
